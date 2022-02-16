14eb502d8c1212be7d9d5764624931fe18ba466a
MSGuy01 <47047931+MSGuy01@users.noreply.github.com>
2022-02-16 00:15:26 +0000

    Finished all motion

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+var totalP = 0;
 /*
 {
   gameCode: {
     players: [bob, joe],
-    online: 1
+    online: 1,
+    policePos: [],
   }
 }
 */
@@ -21,25 +23,38 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  console.log(Object.keys(socket.client.nsps));
+  totalP++;
+  io.emit('connection', totalP);
 });
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
 io.on('connection', (socket) => {
-  socket.on('movement', (x, y) => {
-    console.log('new pos: (' + x + ',' + y + ')');
+  socket.on('disconnect', (socket) => {
+    console.log('a user disconnected');
+    totalP--;
+    io.emit('connection', totalP)
+  });
+  socket.on('movement', (x,y,code,playerNum) => {
+    games[code].policePos = [x,y];
+    //change this for thief/police switching
+    let type = playerNum;
+    io.emit('movement',code,x,y,playerNum,type);
   });
   socket.on('join', (code, name) => {
     let gamesArr = Object.keys(games);
     if (gamesArr.includes(code)) {
       games[code].players.push(name);
       games[code].online++;
+      io.emit('code',code);
     }
     else {
-      console.log('does not exist');
+      io.emit('code','Does Not Exist');
     }
   });
+  socket.on('exist', () => {
+    io.emit('exist');
+  })
   socket.on('create', (name) => {
     //TODO:PREVENT DUPLICATE CODES
     let moveOn = false;
@@ -58,7 +73,8 @@ io.on('connection', (socket) => {
     }
     games[code] = {
       'players': [name],
-      'online': 1
+      'online': 1,
+      'policePos': [64,32]
     };
     io.emit('code', code);
   })
